4c680119af6aeb4bf9e08dbbe9f7fd5509e2493b
Steve Frend <stevefrend1@gmail.com>
2020-05-07 19:54:36 -0700

    Trying to add redis implementation to directives. Partially working

--- server/rateLimiter.ts
+++ server/rateLimiter.ts
@@ -7,96 +7,109 @@ import {
   GraphQLError,
   GraphQLSchema,
   GraphQLType,
-  GraphQLNamedType
+  GraphQLNamedType,
 } from 'graphql';
 import { type } from 'os';
+const asyncRedis = require('async-redis');
+const redis = require('redis');
+const client = asyncRedis.createClient();
 
-// let count: number = 5;
+// Redis Rate Limiter -------------------------------------------
+const rateLimiter = async (limit: number, ip: string, scope: string) => {
+  const expirationTimeVariable = 10; // NEED TO CHANGE
+  const key = ip + '_' + scope;
 
-// export const rateLimiter = (requestContext: any, responseContext: any, options: any = {
-//   requestLimit: 10,
-//   throttleSetting: null,
-//   timer: 20,
-// }) => {
-//   count++;
-//   console.log(requestContext.context.req.ip)
-//   if (count >= options.requestLimit) {
-//     throw new Error(`Request limit exceeded. Try again in ${options.timer} minutes`)
-//   }
-//   console.log(count)
-// }
+  let exists = await client.exists(key);
 
-const rateLimiter = (limit: number) => {
-  let count: number = 0;
-  return function () {
-    if (count === limit) {
-      throw new Error("nope, you've exceeded your requests");
+  if (exists === 0) {
+    await client.setex(key, expirationTimeVariable, 1);
+  } else {
+    await client.incr(key);
+    let value = await client.get(key);
+    value = Number(value);
+
+    await console.log('CURRENT VALUE', value);
+
+    console.log('value,', value, 'limit', limit);
+    if (value > limit) {
+      console.log('value,', value, 'limit', limit);
+      return false;
     }
-    count += 1;
-    console.log(count);
-  };
+    return true;
+  }
 };
+//---------------------------------------------------------------
 
 export class portaraSchemaDirective extends SchemaDirectiveVisitor {
-  
-  
-
-  visitFieldDefinition(field: GraphQLField<any, any>, details) {  
+  visitFieldDefinition(field: GraphQLField<any, any>, details) {
     const { limit } = this.args;
-    const fields = details.objectType.getFields();
-    
-    const variables = {};
-    for (let field in fields) {
-      variables[field] = fields[field].resolve;
-      variables[`rateLimiter-${field}`] = rateLimiter(limit);
-    }
+    const { resolve = defaultFieldResolver } = field;
+    //   const fields = details.objectType.getFields();
+    //   console.log('FIELDS:', fields.hello)
+    //   // console.log('PROTO:', fields.hello.resolve.__proto__.length)
+    //   // console.log('O.GPO:', Object.getPrototypeOf(fields.hello.resolve))
+    //   // console.log(Object.getOwnPropertyNames(fields.hello.resolve))
+    //   const variables = {};
+    //   for (let field in fields) {
+    //     variables[field] = fields[field].resolve;
+    //     // variables[`rateLimiter-${field}`] = rateLimiter(limit);
+    //   }
+    //   field.resolve = (object, args, context, info) => {
+    //     console.log(context.req.ip)
+    //     variables[`rateLimiter-${info.fieldName}`]();
+    //     return variables[info.fieldName]();
+    //   };
+    field.resolve = async (...ogArgs) => {
+      const [object, args, context, info] = ogArgs;
+      const underLimit = await rateLimiter(limit, context.req.ip, info.fieldName);
+      console.log('LINE 101 RANDOMSAOIDN');
 
-    field.resolve = (object, args, context, info) => {
-      variables[`rateLimiter-${info.fieldName}`]();
-      return variables[info.fieldName]();
+      if (!underLimit) {
+        return resolve(...ogArgs);
+      } else return new Error('Over Limit');
     };
   }
-  visitObject(type: GraphQLObjectType) {
-    const { limit } = this.args;
-    const fields = type.getFields();
-    const variables = {};
-    const func = rateLimiter(limit);
-
-    Object.values(fields).forEach((field) => {
-      if (!field.astNode!.directives!.some((directive) => directive.name.value === 'portara')) {
-        variables[field.name] = field.resolve;
-        field.resolve = (object, args, context, info) => {
-          func();
-          return variables[field.name]();
-        };
-      }
-    });
-  }
-  visitSchema(schema: GraphQLSchema) {
-    const { limit } = this.args;
-    const func = rateLimiter(limit);
-    // Store all root types in an object
-    const allTypes = {}
-
-    Object.assign(allTypes, schema.getQueryType()?.getFields(), schema.getMutationType()?.getFields(), schema.getSubscriptionType()?.getFields())
-    
-    
-    Object.values(allTypes).forEach((field:any) => {
-      // console.log(field)
-      // if () 
-      if (!field.astNode!.directives!.some((directive) => directive.name.value === 'portara')) {
-        if (field.resolve) {
-          allTypes[field.name] = field.resolve;
-          field.resolve = (object, args, context, info) => {
-            func();
-            return allTypes[field.name]();
-          };
-        }
-      }
-    })
-
-    
-    
-  }
 }
-
+// visitObject(type: GraphQLObjectType) {
+//   const { limit } = this.args;
+//   const fields = type.getFields();
+//   const variables = {};
+//   const func = rateLimiter(limit);
+//   Object.values(fields).forEach((field) => {
+//     if (!field.astNode!.directives!.some((directive) => directive.name.value === 'portara')) {
+//       variables[field.name] = field.resolve;
+//       field.resolve = (object, args, context, info) => {
+//         func();
+//         return variables[field.name]();
+//       };
+//     }
+//   });
+// }
+// visitSchema(schema: GraphQLSchema) {
+//   const { limit } = this.args;
+//   const func = rateLimiter(limit);
+//   // Store all root types in an object
+//   const allTypes = {}
+//   Object.assign(allTypes, schema.getQueryType()?.getFields(), schema.getMutationType()?.getFields(), schema.getSubscriptionType()?.getFields())
+//   Object.values(allTypes).forEach((field:any) => {
+//     // console.log(field)
+//     // if ()
+//     if (!field.astNode!.directives!.some((directive) => directive.name.value === 'portara')) {
+//       if (field.resolve) {
+//         allTypes[field.name] = field.resolve;
+//         field.resolve = (object, args, context, info) => {
+//           func();
+//           return allTypes[field.name]();
+//         };
+//       }
+//     }
+//   })
+//   }
+// }
+// Redis Connection:
+/*
+Endpoint:
+redis-11068.c98.us-east-1-4.ec2.cloud.redislabs.com:11068
+In Terminal:
+redis-cli -h redis-11068.c98.us-east-1-4.ec2.cloud.redislabs.com -p 11068 -a cats35_ql
+*/
--- server/server.ts
+++ server/server.ts
@@ -1,64 +1,55 @@
 const express = require('express');
-const graphql = require('graphql')
+const graphql = require('graphql');
 const { ApolloServer } = require('apollo-server-express');
 const { gql } = require('apollo-server');
 // import { rateLimiter } from './rateLimiter'
-import { portaraSchemaDirective } from './rateLimiter'
-
+import { portaraSchemaDirective } from './rateLimiter';
 // Types
 const typeDefs = gql`
-  directive @portara(limit: Int!) on FIELD_DEFINITION | OBJECT | SCHEMA
-
-  schema  @portara(limit: 25) {
+  directive @portara(limit: Int!) on FIELD_DEFINITION
+  schema {
     query: Query
     mutation: Mutation
   }
-
   type Query {
     test: String!
   }
-
   type Mutation {
-    hello: String! # @portara(limit: 2)
-    bye: String! #@portara(limit: 2)
+    hello: String! @portara(limit: 2)
+    bye: String! @portara(limit: 2)
   }
 `;
- 
 // Resolvers
 const resolvers = {
   Mutation: {
     hello: (parent, args, context, info) => {
-      return 'Request completed and returned'
+      return 'Request completed and returned';
     },
     bye: (parent, args, context, info) => {
-      return 'Goodbye world'
-    }
+      return 'Goodbye world';
+    },
   },
 };
-
-
 // Express & Apollo setup
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({req, res}) => ({req, res}),
+  context: ({ req, res }) => ({ req, res }),
   // plugins: [
   //   {
   //     requestDidStart(requestContext, responseContext) {
   //       rateLimiter(requestContext, responseContext)
-  //     },   
+  //     },
   //   }
   // ],
   schemaDirectives: {
     portara: portaraSchemaDirective,
   },
-})
-
+});
 server.applyMiddleware({
   app,
 });
-
-app.listen({port: 4000}, () => {
-  console.log(`Server running @ PORT 4000`)
-})
+app.listen({ port: 4000 }, () => {
+  console.log(`Server running @ PORT 4000`);
+});

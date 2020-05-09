50a2512930fcc754b57af5c45b28f9463d198fa9
Alexander Infante <alexinfante17@gmail.com>
2020-05-09 10:38:21 -0700

    Per Branch: implementing the time window for developers to specify the expiration time of the requests from Redis. Added new functions that result in the same expiration time variable name, no merge conflicts exist.

--- server/rateLimiter.ts
+++ server/rateLimiter.ts
@@ -15,8 +15,37 @@ const redis = require('redis');
 const client = asyncRedis.createClient();
 
 // Redis Rate Limiter -------------------------------------------
-const rateLimiter = async (limit: number, ip: string, scope: string) => {
-  const expirationTimeVariable = 20; // NEED TO CHANGE
+const rateLimiter = async (limit: number, per: string, ip: string, scope: string) => {
+
+  // Per Functionality ---------------------------
+
+  const perNum = Number(per.match(/\d+/g)?.toString()) 
+  const perWord = per.match(/[a-zA-Z]+/g)?.toString().toLowerCase();
+
+  const timeFrameMultiplier = (timeFrame) => {
+    if (timeFrame === 'milliseconds' || timeFrame === 'millisecond' || timeFrame === 'mil' || timeFrame === 'mils') {
+      return 0.001;
+    } else if (timeFrame === 'seconds' || timeFrame === 'second' || timeFrame === 'sec' || timeFrame === 'secs') {
+      return 1;
+    } else if (timeFrame === 'minutes' || timeFrame === 'minute' || timeFrame === 'min' || timeFrame === 'mins') {
+      return 60;
+    } else if (timeFrame === 'hours' || timeFrame === 'hour' || timeFrame === 'h') {
+      return 60 * 60;
+    } else if (timeFrame === 'days' || timeFrame === 'day' || timeFrame === 'd') {
+      return 60 * 60 * 24;
+    } else if (timeFrame === 'weeks' || timeFrame === 'week' || timeFrame === 'w') {
+      return 60 * 60 * 24 * 7
+    } else if (timeFrame === '' || timeFrame === undefined) {
+      return 1;
+    } else {
+      return new Error('Not a valid measure of time!');
+  }
+}
+
+  // get final result of expirationTimeVariable
+  let expirationTimeVariable = ( <number> timeFrameMultiplier(perWord) * perNum);
+  // ---------------------------------------------
+
   const key = ip + '_' + scope;
 
   let exists = await client.exists(key);
@@ -38,11 +67,12 @@ export class portaraSchemaDirective extends SchemaDirectiveVisitor {
 
   visitFieldDefinition(field: GraphQLField<any, any>, details) {
     const { limit } = this.args;
+    const { per } = this.args;
     const { resolve = defaultFieldResolver } = field;
 
     field.resolve = async (...originalArgs) => {
       const [object, args, context, info] = originalArgs;
-      const underLimit = await rateLimiter(limit, context.req.ip, info.fieldName);
+      const underLimit = await rateLimiter(limit, per, context.req.ip, info.fieldName);
       if (underLimit) {
         return resolve( ...originalArgs);
       } else return new Error('Over Limit');
@@ -52,6 +82,7 @@ export class portaraSchemaDirective extends SchemaDirectiveVisitor {
 
   visitObject(type: GraphQLObjectType) {
     const { limit } = this.args;
+    const { per } = this.args;
     const fields = type.getFields();
     // console.log(fields)
 
@@ -62,7 +93,7 @@ export class portaraSchemaDirective extends SchemaDirectiveVisitor {
 
         field.resolve = async (...originalArgs) => {
           const [object, args, context, info] = originalArgs;
-          const underLimit = await rateLimiter(limit, context.req.ip, type.toString());
+          const underLimit = await rateLimiter(limit, per, context.req.ip, type.toString());
           if (underLimit) {
             return resolve(...originalArgs);
           } else return new Error('Over Limit');

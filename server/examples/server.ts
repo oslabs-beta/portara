afd4372ffe49529e5ab1726cfa20b4e39ff521fd
Alexander Infante <alexinfante17@gmail.com>
2020-05-11 16:21:52 -0700

    Adding the correct files to the npm package for easy download. Adding the README and LICENSE files to our npm as well as connecting to our Github Repo

---
+++ server/examples/server.ts
@@ -0,0 +1,45 @@
+const { ApolloServer, gql } = require('apollo-server');
+import { portaraSchemaDirective } from '../rateLimiter';
+
+// typeDefs
+const typeDefs = gql`
+  directive @portara(limit: Int!, per: ID!) on FIELD_DEFINITION | OBJECT 
+
+  type Query {
+    test: String!
+  }
+  type Mutation  @portara(limit: 8, per: 10){
+    hello: String! @portara(limit: 2, per: "10")
+    bye: String! #@portara(limit: 2)
+  }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    test: (parent, args, context, info) => {
+      return 'Test'
+    }
+  },
+  Mutation: {
+    hello: (parent, args, context, info) => {
+      return 'Request completed and returned';
+    },
+    bye: (parent, args, context, info) => {
+      return 'Goodbye world';
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req, res }) => ({ req, res }),
+  schemaDirectives: {
+    portara: portaraSchemaDirective,
+  },
+});
+
+server.listen({ port: 4000 }, () => {
+  console.log(`Server running @ PORT 4000`);
+});

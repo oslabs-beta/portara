52ab3ebfe2265ce9afc8ab9f29b7870a1ec9e2c2
Todd Alexander <60899614+toddalex@users.noreply.github.com>
2020-05-13 23:55:48 -0700

    adding styles + timeMult tests

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import Test from './Test';
-import './App.css';
+
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
@@ -12,7 +12,6 @@ function App(): JSX.Element {
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        <h1>Portara</h1>
         <Test />
       </div>
     </ApolloProvider>
--- server/__tests__/rateLimit.test.ts
+++ server/__tests__/rateLimit.test.ts
@@ -67,15 +67,25 @@ describe('Receives a response from our GraphQL Query', () => {
   })
 })
 
-describe('Rate Limiter accepts various timeframe values', () => {
+describe('timeframe multiplier tests', () => {
   it('returns an error when input value is not recognized', () => {
-    const timeframe = timeFrameMultiplier('years')
-    expect(timeframe).toBeInstanceOf(Error)
+    const timeFrame = timeFrameMultiplier('years')
+    expect(timeFrame).toBeInstanceOf(Error)
   })
 
-  it('defaults to 1 second when value is an empty string', ()=> {
-    const timeframe = timeFrameMultiplier('')
-    expect(timeframe).toEqual(1000)
+  it('defaults to 1 second when value is an empty string or undefined', ()=> {
+    const timeFrame = timeFrameMultiplier(undefined || '')
+    expect(timeFrame).toEqual(1000)
+  })
+
+  it('converts hours into milliseconds if the input is hours', ()=> {
+    const timeFrame = timeFrameMultiplier('hours')
+    expect(timeFrame).toEqual(3600000)
+  })
+
+  it('converts days into milliseconds if the input is days', ()=> {
+    const timeFrame = timeFrameMultiplier('days')
+    expect(timeFrame).toEqual(86400000)
   })
 })
 
